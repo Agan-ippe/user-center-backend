d0da8c92873133ef6148febe5a55355e64abbd48
Agan-ippe <wjy7136@163.com>
2023-10-15 10:10:54 +0800

    更新了一些Bean对象

---
+++ src/main/java/com/aip/usercenter/bean/User.java
@@ -0,0 +1,82 @@
+package com.aip.usercenter.bean;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author aip
+ * 用户表
+ * @TableName t_user
+ */
+@TableName(value ="t_user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements Serializable {
+    /**
+     * id
+     */
+    @TableId(type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 用户昵称
+     */
+    private String username;
+
+    /**
+     * 用户登录账号
+     */
+    private String userAccount;
+
+    /**
+     * 登录密码
+     */
+    private String pwd;
+
+    /**
+     * 用户性别
+     */
+    private Integer gender;
+
+    /**
+     * 用户头像
+     */
+    private String avatarUrl;
+
+    /**
+     * 邮箱
+     */
+    private String email;
+
+    /**
+     * 用户状态，0-正常
+     */
+    private Integer userStatus;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+
+    /**
+     * 更新时间
+     */
+    private Date updateTime;
+
+    /**
+     * 是否删除，为逻辑删除。0 - 正常；1 - 删除
+     */
+    private Integer deleted;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
