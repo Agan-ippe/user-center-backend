a8b7565b9b803ec66084735f688b6233210204b4
Agan-ippe <wjy7136@163.com>
2023-10-18 16:04:14 +0800

    更新了一些配置

--- src/main/java/com/aip/usercenter/bean/User.java
+++ src/main/java/com/aip/usercenter/bean/User.java
@@ -1,9 +1,7 @@
 package com.aip.usercenter.bean;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.*;
+
 import java.io.Serializable;
 import java.util.Date;
 
@@ -75,6 +73,7 @@ public class User implements Serializable {
     /**
      * 是否删除，为逻辑删除。0 - 正常；1 - 删除
      */
+    @TableLogic
     private Integer deleted;
 
     @TableField(exist = false)
